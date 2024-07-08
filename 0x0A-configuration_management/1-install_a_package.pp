950de442e450ff75f524a2a4bd1d2621bd2cce7d
AymanChatki <aymanchatki4@gmail.com>
2024-07-08 18:03:21 +0100

    Modify 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-#INSTALL flask from pip3
+# Ensure Python is installed
+package { 'python3':
+  ensure => installed,
+}
+
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure  => installed,
+  require => Package['python3'],
+}
+
+# Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
