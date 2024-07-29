1fc15b7dd93d88d849430d1e1e2b4ac64c1a7384
ChatkiAyman <aymanchatki@gmail.com>
2024-07-29 22:51:37 +0100

    Add pycodestyle

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""script to export data in the CSV format"""
-"""run file with ./1-export_to_CSV.py 2"""
 import csv
 import requests
 from sys import argv
+"""script to export data in the CSV format"""
+"""run file with ./1-export_to_CSV.py 2"""
 
 
 def main():
